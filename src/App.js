af4f574f1cacbdc49f6417866c8de3a4bf0bec23
mjrgeorge <mjrgeorge@gmail.com>
2020-09-16 07:39:32 +0600

    setup complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div>
+      <h1 className = "bg-dark">Hello World!</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
