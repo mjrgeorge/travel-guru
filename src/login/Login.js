d8bc57c508f91112fb82639f26216a24789c5076
mjrgeorge <mjrgeorge@gmail.com>
2020-09-17 00:31:21 +0600

    somthing update

--- src/login/Login.js
+++ src/login/Login.js
@@ -12,9 +12,13 @@ const Login = () => {
     const [user, setUser] = useState({
         isSignedIn: false,
         name: '',
+        firstName: '',
+        lastName: '',
         email: '',
         password: '',
         photo: '',
+        success: '',
+        error: '',
     });
 
     const provider = new firebase.auth.GoogleAuthProvider();
@@ -33,7 +37,7 @@ const Login = () => {
           .catch(error=>{
               console.log(error);
           });
-    }
+    };
 
     const handleSignOut = () => {
         firebase.auth().signOut()
@@ -49,20 +53,68 @@ const Login = () => {
           .catch(error=> {
             console.log(error);
           });
-    }
+    };
+
+    const handleBlur = (e) => {
+        let isFieldValid = true;
+        if(e.target.name==="email"){
+            isFieldValid = /\S+@\S+\.+/.test(e.target.value);
+        }
+        if(e.target.name==="password"){
+            const isPasswordValid = e.target.value.length > 6;
+            const passwordHasNumber = /\d{1}/.test(e.target.value);
+            isFieldValid = isPasswordValid && passwordHasNumber;
+        }
+        if(isFieldValid){
+            const newUserInfo = {...user};
+            newUserInfo[e.target.name] = e.target.value;
+            setUser(newUserInfo);
+        }
+    };
+
+    const handleSubmit = (e) => {
+        if(user.firstName&&user.lastName&&user.email&&user.password){
+            firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+            .then(response=>{
+                const newUserInfo = {...user};
+                newUserInfo.error = '';
+                newUserInfo.success = true;
+                setUser(newUserInfo);
+            })
+            .catch(error=> {
+                const newUserInfo = {...user};
+                newUserInfo.error = error.message;
+                newUserInfo.success = false;
+                setUser(newUserInfo);
+              });
+        }
+
+        e.preventDefault();
+    };
 
 
     return (
 
         <div>
-            <h1>Name: {user.name} </h1>
-            {user.isSignedIn?
+            <div>
+                <h1>Name: {user.firstName} {user.lastName}</h1>
+                <h1>Email: {user.email} </h1>
+                <h1>Password: {user.password} </h1>
+                {
+                user.success && <p style={{color: 'green'}}> User Created Successfully</p>
+                }
+                {
+                user.error && <p style={{color: 'red'}}> {user.error} </p>
+                }
+
+            </div>
+            {/* {user.isSignedIn?
                 <div>
                     <Button onClick={handleSignOut} variant="warning" block>Log Out</Button>
                 </div>
-                :
+                : */}
                 <Card style={{width: '500px', padding: '25px', margin: '100px auto'}}>
-                <div>
+                {/* <div>
                     <form>
                         <h3>Login</h3>
                         <br/>
@@ -81,23 +133,23 @@ const Login = () => {
                             <p className="pr-2">Don't have an account?</p> <Link to="">Create an account</Link>
                         </div>
                     </form>
-                </div>
+                </div> */}
                 <div>
                     <Form>
                         <h3>Create an account</h3>
                         <br/>
                         <Form.Group>
-                            <Form.Control type="text" placeholder = "First Name"/>
+                            <Form.Control onBlur={handleBlur} name="firstName" type="text" placeholder = "First Name" required/>
                             <br/>
-                            <Form.Control type="text" placeholder = "Last Name"/>
+                            <Form.Control onBlur={handleBlur} name="lastName" type="text" placeholder = "Last Name" required/>
                             <br/>
-                            <Form.Control type="email" placeholder = "Email"/>
+                            <Form.Control onBlur={handleBlur} name="email" type="email" placeholder = "Email" required/>
                             <br/>
-                            <Form.Control type="password" placeholder = "Password"/>
-                            <br/>
-                            <Form.Control type="password" placeholder = "Confirm Password"/>
+                            <Form.Control onBlur={handleBlur} name="password" type="password" placeholder = "Password" required/>
+                            {/* <br/>
+                            <Form.Control onBlur={handleBlur} name="" type="confirmPassword" placeholder = "Confirm Password" required/> */}
                         </Form.Group>
-                            <Button variant="warning" type="submit" block>Create an account</Button>
+                            <Button onClick={handleSubmit} variant="warning" type="submit" block>Create an account</Button>
                             <br/>
                             <p className="text-center">Already have an account? <Link to="">Login</Link> </p>
                     </Form>
@@ -110,7 +162,7 @@ const Login = () => {
                     <Button onClick={handleSignIn} variant="info" block>Continue With Google</Button>
                 </div>
             </Card>
-            }
+            {/* } */}
         </div>
     );
 };
