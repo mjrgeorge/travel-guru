c20aa32ac570bb7777f4f187e956b3c91bb6ef29
mjrgeorge <mjrgeorge@gmail.com>
2020-09-18 23:15:19 +0600

    ready for submit

--- src/components/notMatch/NotMatch.js
+++ src/components/notMatch/NotMatch.js
@@ -3,7 +3,8 @@ import React from 'react';
 const NotMatch = () => {
     return (
         <div>
-            <h2>404 Error; Sorry Your Page Is Not Found</h2>
+            <h2 className="display-1 m-5 p-5 text-warning text-center">404 Error!</h2>
+            <p className="display-4 text-info text-center">Sorry, Your Page Is Not Found!</p>
         </div>
     );
 };
